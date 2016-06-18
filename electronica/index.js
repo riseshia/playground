19789e47cac8419503d61f34ac82bb9af000f1a4
Shia <rise.shia@gmail.com>
2016-06-18 21:55:49 +0900

    Eletronica

---
+++ electronica/index.js
@@ -0,0 +1,37 @@
+'use strict';
+const electron = require('electron');
+const {crashReporter} = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+// Report crashes to our server.
+crashReporter.start({
+  productName: 'YourName',
+  companyName: 'YourCompany',
+  submitURL: 'https://your-domain.com/url-to-submit',
+  autoSubmit: true
+});
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
