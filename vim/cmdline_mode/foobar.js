b16b82427671cc67b48dac1baa174e8d27f59ffe
Shia <rise.shia@gmail.com>
2016-07-30 17:54:57 +0900

    cmdlineMode

---
+++ vim/cmdline_mode/foobar.js
@@ -0,0 +1,6 @@
+A;<Esc> jVG :'<,'>normal. or :%normal A;
+var foo = 1
+var bar = 'a'
+var baz = 'z'
+var foobar = foo + bar
+var foobarbaz = foo + bar + baz
---
+++ vim/cmdline_mode/loop.js
@@ -0,0 +1,5 @@
+cwcounter<Esc> :%s//<C-r><C-w>/g
+var tally;
+for (counter=1; tally <= 10; tally++) {
+  // do something with tally
+};
