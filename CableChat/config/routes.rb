789a6fd6ecf04d41d959f676e6ad8059a784b371
Shia <rise.shia@gmail.com>
2017-02-23 17:07:58 +0900

    ActionCable sample

--- CableChat/config/routes.rb
+++ CableChat/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  root 'chat_rooms#index'
+
+  mount ActionCable.server => '/cable'
 end
