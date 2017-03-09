6fb4b61eb1ab4f1de21f886ce637c663e47ef748
Shia <rise.shia@gmail.com>
2017-03-09 09:57:10 +0900

    Django-tutorial - 7: Customize Admin

--- django-tutorial/polls/models.py
+++ django-tutorial/polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -10,6 +11,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
