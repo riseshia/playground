2ef861ce477785153bfc6667fe9fbfb70532ffb5
Shia <rise.shia@gmail.com>
2021-05-14 20:36:34 +0900

    [rshell] First impl
    Ref: https://www.joshmcguigan.com/blog/build-your-own-shell-rust/

---
+++ rshell/src/main.rs
@@ -0,0 +1,70 @@
+use std::io::{Write, stdin, stdout};
+use std::path::Path;
+use std::process::{Command, Child, Stdio};
+use std::env;
+
+fn main() {
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
+
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next();
+            if None == command {
+                continue;
+            }
+
+            let command = command.unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                },
+                "exit" => return,
+                command => { // External command
+                    let stdin = previous_command
+                        .map_or(
+                            Stdio::inherit(), |output: Child| Stdio::from(output.stdout.unwrap())
+                        );
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => { previous_command = Some(output); },
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        },
+                    }
+                }
+            }
+        }
+
+        if let Some(mut final_command) = previous_command {
+            final_command.wait().unwrap();
+        }
+    }
+}
