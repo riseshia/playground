185565a41019072abbf8f366422f181dc0618603
Shia <rise.shia@gmail.com>
2022-10-20 20:05:56 +0900

    Reset cursor position

--- pound_editor/src/main.rs
+++ pound_editor/src/main.rs
@@ -1,12 +1,78 @@
-use crossterm::event::{Event, KeyCode, KeyEvent};
-use crossterm::{event, terminal};
+use crossterm::event::*;
+use crossterm::terminal::ClearType;
+use crossterm::{cursor, event, execute, terminal};
+use std::io::stdout;
 use std::time::Duration;
 
 struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
-        terminal::disable_raw_mode().expect("Could not disable raw mode")
+        terminal::disable_raw_mode().expect("Could not disable raw mode");
+        Output::clear_screen().expect("Clear screen error")
+    }
+}
+
+struct Output;
+
+impl Output {
+    fn new() -> Self {
+        Self
+    }
+
+
+    fn clear_screen() -> crossterm::Result<()> {
+        execute!(stdout(), terminal::Clear(ClearType::All))?;
+        execute!(stdout(), cursor::MoveTo(0, 0))
+    }
+
+    fn refresh_screen(&self) -> crossterm::Result<()> {
+        Self::clear_screen()
+    }
+}
+
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> crossterm::Result<KeyEvent> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read()? {
+                    return Ok(event);
+                }
+            }
+        }
+    }
+}
+
+struct Editor {
+    reader: Reader,
+    output: Output,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self {
+            reader: Reader,
+            output: Output::new()
+        }
+    }
+
+    fn process_keypress(&self) -> crossterm::Result<bool> {
+        match self.reader.read_key()? {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: event::KeyModifiers::CONTROL,
+                ..
+            } => return Ok(false),
+            _ => {}
+        }
+        Ok(true)
+    }
+
+    fn run(&self) -> crossterm::Result<bool> {
+        self.output.refresh_screen()?;
+        self.process_keypress()
     }
 }
 
@@ -15,24 +81,8 @@ fn main() -> crossterm::Result<()> {
 
     terminal::enable_raw_mode()?;
 
-    loop {
-        if event::poll(Duration::from_millis(1000))? {
-            if let Event::Key(event) = event::read()? {
-                #[allow(clippy::single_match)] match event {
-                    KeyEvent {
-                        code: KeyCode::Char('q'),
-                        modifiers: event::KeyModifiers::NONE,
-                        ..
-                    } => break,
-                    _ => {
-                        //todo
-                    }
-                }
-                println!("{:?}\r", event);
-            }
-        } else {
-            println!("No input yet\r");
-        }
-    }
+    let editor = Editor::new();
+    while editor.run()? {}
+
     Ok(())
 }
