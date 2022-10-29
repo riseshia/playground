acc63cd935a3043ad104d2d706a0512ad38797ce
Shia <rise.shia@gmail.com>
2022-10-29 16:53:42 +0900

    Setup default display

--- pound_editor/src/main.rs
+++ pound_editor/src/main.rs
@@ -1,8 +1,10 @@
 use crossterm::event::*;
 use crossterm::terminal::ClearType;
-use crossterm::{cursor, event, execute, terminal};
-use std::io::stdout;
+use crossterm::{queue, cursor, event, execute, terminal};
+use std::io::{stdout, Write, self};
 use std::time::Duration;
+
+const VERSION: &str = "0.0.1";
 
 struct CleanUp;
 
@@ -13,21 +15,114 @@ impl Drop for CleanUp {
     }
 }
 
-struct Output;
+struct EditorContents {
+    content: String,
+}
+
+impl EditorContents {
+    fn new() -> Self {
+        Self {
+            content: String::new(),
+        }
+    }
+
+    fn push(&mut self, ch: char) {
+        self.content.push(ch)
+    }
+
+    fn push_str(&mut self, string: &str) {
+        self.content.push_str(string)
+    }
+}
+
+impl io::Write for EditorContents {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match std::str::from_utf8(buf) {
+            Ok(s) => {
+                self.content.push_str(s);
+                Ok(s.len())
+            }
+            Err(_) => Err(io::ErrorKind::WriteZero.into()),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        let out = write!(stdout(), "{}", self.content);
+        stdout().flush()?;
+        self.content.clear();
+        out
+    }
+}
+
+struct Output {
+    win_size: (usize, usize),
+    editor_contents: EditorContents,
+}
 
 impl Output {
     fn new() -> Self {
-        Self
-    }
+        let win_size = terminal::size()
+            .map(|(x, y)| (x as usize, y as usize))
+            .unwrap();
 
+        Self {
+            win_size,
+            editor_contents: EditorContents::new(),
+        }
+    }
 
     fn clear_screen() -> crossterm::Result<()> {
         execute!(stdout(), terminal::Clear(ClearType::All))?;
         execute!(stdout(), cursor::MoveTo(0, 0))
     }
 
-    fn refresh_screen(&self) -> crossterm::Result<()> {
-        Self::clear_screen()
+    fn draw_rows(&mut self) {
+        let screen_rows = self.win_size.1;
+        let screen_columns = self.win_size.0;
+
+        for i in 0..screen_rows {
+            if i == screen_rows / 3 {
+                let mut welcome = format!("Pound Editor --- Version {}", VERSION);
+                if welcome.len() > screen_columns {
+                    welcome.truncate(screen_columns)
+                }
+
+                let mut padding = (screen_columns - welcome.len()) / 2;
+                if padding != 0 {
+                    self.editor_contents.push('~');
+                    padding -= 1;
+                }
+                (0..padding).for_each(|_| self.editor_contents.push(' '));
+                self.editor_contents.push_str(&welcome);
+            } else {
+                self.editor_contents.push('~');
+            }
+
+            queue!(
+                self.editor_contents,
+                terminal::Clear(ClearType::UntilNewLine)
+            )
+            .unwrap();
+
+            if i < screen_rows - 1 {
+                self.editor_contents.push_str("\r\n");
+            }
+        }
+    }
+
+    fn refresh_screen(&mut self) -> crossterm::Result<()> {
+        queue!(
+            self.editor_contents,
+            cursor::Hide,
+            cursor::MoveTo(0, 0)
+        )?;
+        self.draw_rows();
+        queue!(
+            self.editor_contents,
+            cursor::MoveTo(0, 0),
+            cursor::Show
+        )?;
+        self.editor_contents.flush()
     }
 }
 
@@ -70,7 +165,7 @@ impl Editor {
         Ok(true)
     }
 
-    fn run(&self) -> crossterm::Result<bool> {
+    fn run(&mut self) -> crossterm::Result<bool> {
         self.output.refresh_screen()?;
         self.process_keypress()
     }
@@ -81,7 +176,7 @@ fn main() -> crossterm::Result<()> {
 
     terminal::enable_raw_mode()?;
 
-    let editor = Editor::new();
+    let mut editor = Editor::new();
     while editor.run()? {}
 
     Ok(())
