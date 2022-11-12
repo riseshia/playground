e7bab49abd91e3f653fe2f830963d7cc8e0c4e44
Shia <rise.shia@gmail.com>
2022-11-12 13:13:08 +0900

    Moving cursor to start of the next line
    which reaches to end of a line

--- pound_editor/src/lib.rs
+++ pound_editor/src/lib.rs
@@ -1,6 +1,7 @@
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers, Event};
 use crossterm::terminal::ClearType;
 use crossterm::{queue, cursor, event, execute, terminal};
+use std::cmp::Ordering;
 use std::io::{stdout, Write, self};
 use std::path::Path;
 use std::time::Duration;
@@ -46,7 +47,12 @@ impl CursorController {
                 self.cursor_y = self.cursor_y.saturating_sub(1);
             }
             KeyCode::Left => {
-                self.cursor_x = self.cursor_x.saturating_sub(1);
+                if self.cursor_x != 0 {
+                    self.cursor_x -= 1;
+                } else if self.cursor_y > 0 {
+                    self.cursor_y -= 1;
+                    self.cursor_x = editor_rows.get_row(self.cursor_y).len();
+                }
             }
             KeyCode::Down => {
                 if self.cursor_y != number_of_rows {
@@ -54,8 +60,15 @@ impl CursorController {
                 }
             }
             KeyCode::Right => {
-                if self.cursor_y < number_of_rows && self.cursor_x < editor_rows.get_row(self.cursor_y).len() {
-                    self.cursor_x += 1;
+                if self.cursor_y < number_of_rows {
+                    match self.cursor_x.cmp(&editor_rows.get_row(self.cursor_y).len()) {
+                        Ordering::Less => self.cursor_x += 1,
+                        Ordering::Equal => {
+                            self.cursor_y += 1;
+                            self.cursor_x = 0
+                        }
+                        _ => {}
+                    }
                 }
             }
             KeyCode::Home => {
