43075d1a562e8c0bd2273f4847d1c25c792e4a90
Shia <rise.shia@gmail.com>
2022-11-13 20:30:57 +0900

    rust cli example

---
+++ grrs/src/main.rs
@@ -0,0 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
