aa8526dd2e0029cabef7f5d363f395d4cf53ea07
Shia <rise.shia@gmail.com>
2022-11-13 15:51:35 +0900

    [actix-web-blog] Impl POST /posts

---
+++ actix-web-blog/src/repository.rs
@@ -0,0 +1,66 @@
+use crate::schema::*;
+use diesel::{Insertable, Queryable};
+use diesel::r2d2::{self, ConnectionManager};
+use diesel::sqlite::SqliteConnection;
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+use actix_web::web;
+
+use crate::error::ApiError;
+
+type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+#[derive(Deserialize, Insertable)]
+#[diesel(table_name = posts)]
+pub struct NewPost {
+    title: String,
+    body: String,
+}
+
+#[derive(Serialize, Queryable)]
+pub struct Post {
+    id: i32,
+    title: String,
+    body: String,
+    published: bool,
+}
+
+pub struct Repository {
+    pool: DbPool,
+}
+
+impl Repository {
+    pub fn new(database_url: &str) -> Self {
+        let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+        let pool = r2d2::Pool::builder().build(manager).expect("Failed to create a pool.");
+
+        Self { pool }
+    }
+
+    pub async fn create_post(
+        &self,
+        new_post: NewPost,
+    ) -> Result<Post, ApiError> {
+        let mut conn = self.pool.get()?;
+        let post = web::block(move || {
+            diesel::insert_into(posts::table)
+                .values(new_post)
+                .get_result(&mut conn)
+        })
+        .await??;
+
+        Ok(post)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_conn() {
+        let database_url = std::env::var("DATABASE_URL").unwrap();
+        let repo = Repository::new(&database_url);
+        assert!(repo.pool.get().is_ok());
+    }
+}
