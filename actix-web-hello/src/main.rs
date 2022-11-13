fcc3d50f420be142894eac54f254ab8e3b2ee09e
Shia <rise.shia@gmail.com>
2022-11-13 14:58:15 +0900

    [actix-web-hello] Handle params with serde

--- actix-web-hello/src/main.rs
+++ actix-web-hello/src/main.rs
@@ -1,13 +1,30 @@
-use actix_web::{
-    http::header::ContentType, App, HttpResponse,
-    HttpServer,
-};
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct HelloQuery {
+    name: String,
+    age: u32,
+}
+
+#[derive(Serialize)]
+struct HelloResponse {
+    greet: String,
+}
 
 #[actix_web::get("/")]
-async fn hello() -> HttpResponse {
-    HttpResponse::Ok()
-        .append_header(ContentType::json())
-        .body(r#"{ "greet": "Hello, world!" }"#)
+async fn hello(
+    query: web::Query<HelloQuery>,
+) -> HttpResponse {
+    let query = query.into_inner();
+    let message = format!(
+        "Hello, my name is {}! I am {} years old!",
+        query.name, query.age
+    );
+
+    let h = HelloResponse { greet: message };
+
+    HttpResponse::Ok().json(h)
 }
 
 #[actix_web::main]
