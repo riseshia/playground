a811c9e435fa3869bc244e63eeeee8a8385c6e4e
Shia <rise.shia@gmail.com>
2022-12-12 21:44:06 +0900

    Handle jsonnet

--- rust-json-eval/src/main.rs
+++ rust-json-eval/src/main.rs
@@ -1,31 +1,43 @@
-use serde_json::{Result, Value};
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
-fn parse_json() -> Value {
+fn parse_json(data: String) -> Person {
+    serde_json::from_str(&data).expect("Invalid JSON str")
+}
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+use jsonnet::JsonnetVm;
+
+fn main() -> Result<()> {
     let data = r#"
-    {
-        "name": "John Doe",
-        "age": 43,
-        "phones": [
-            "+44 12345678",
-            "+44 22222222"
-
+    local obj = {
+        name: "John Doe",
+        age: 43,
+        phones: [
+            "+44 1234567",
+            "+44 2345678"
         ]
-    }"#;
+    };
 
-    let v: Value = serde_json::from_str(data).expect("Invalid JSON str");
+    obj"#;
 
-    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
+    let mut vm = JsonnetVm::new();
 
-    match v["phones"].get(1) {
+    let output = vm.evaluate_snippet("example", data).unwrap();
+    let person = parse_json(output.as_str().to_string());
+
+    println!("Please call {} at the number {}", person.name, person.phones[0]);
+
+    match person.phones.get(1) {
         Some(phone) => println!("The second phone number is: {}", phone),
         None => println!("There is no second phone number."),
     }
-
-    v
-}
-
-fn main() -> Result<()> {
-    let _json = parse_json();
 
     Ok(())
 }
