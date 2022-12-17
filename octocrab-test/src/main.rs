3b7fbfb4b2d2d09bcc0bdc4d0ee8ddfbb755d595
Shia <rise.shia@gmail.com>
2022-12-17 15:20:14 +0900

    Take out installation repositories

--- octocrab-test/src/main.rs
+++ octocrab-test/src/main.rs
@@ -1,4 +1,4 @@
-use octocrab::models::Repository;
+use octocrab::models::{InstallationToken, InstallationRepositories};
 use octocrab::Octocrab;
 
 #[tokio::main]
@@ -9,11 +9,36 @@ async fn main() -> octocrab::Result<()> {
     let key = jsonwebtoken::EncodingKey::from_rsa_pem(app_private_key.as_bytes()).unwrap();
 
     let octocrab = Octocrab::builder().app(app_id, key).build()?;
-    let _repos: Vec<Repository> = octocrab
-        .get("/installation/repositories", None::<&()>)
+    let installations = octocrab
+        .apps()
+        .installations()
+        .send()
         .await
-        .unwrap();
+        .unwrap()
+        .take_items();
 
+    for ins in installations.iter() {
+        let access: InstallationToken = octocrab
+            .post(
+                ins.access_tokens_url.as_ref().unwrap(),
+                None::<&()>,
+            )
+            .await
+            .unwrap();
+
+        let octocrab = octocrab::OctocrabBuilder::new()
+            .personal_token(access.token)
+            .build()
+            .unwrap();
+
+        let repos: Result<InstallationRepositories, octocrab::Error> = octocrab
+            .get("/installation/repositories", None::<&()>)
+            .await;
+
+        for repo in repos.unwrap().repositories.iter() {
+            println!("{}", repo.name);
+        }
+    }
     Ok(())
 }
 
