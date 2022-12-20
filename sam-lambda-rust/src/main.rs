24c7bcee1024dab7d208f8ad2203582ae8cacf4a
Shia <rise.shia@gmail.com>
2022-12-20 21:23:38 +0900

    Move tests to lib.rs

--- sam-lambda-rust/src/main.rs
+++ sam-lambda-rust/src/main.rs
@@ -8,32 +8,3 @@ async fn main() -> Result<(), Error> {
     lambda_runtime::run(func).await?;
     Ok(())
 }
-
-#[tokio::test]
-async fn test_handle_command() {
-    use serde_json::json;
-    use sam_lambda_rust::{Request, QueryParams, RequestHeader};
-
-    let request_body = QueryParams {
-        user_id: "U11111111".to_owned(),
-        channel_id: "C11111111".to_owned(),
-        command: "/some-command".to_owned(),
-        response_url: "https://hooks.slack.com/commands/1234/5678".to_owned(),
-    };
-
-    let request = Request {
-        raw_path: "/".to_owned(),
-        body: request_body,
-        headers: RequestHeader::default(),
-        is_base64_encoded: false,
-        skip_request_verify: true
-    };
-
-    let context = lambda_runtime::Context::default();
-    let event = lambda_runtime::LambdaEvent::new(request, context);
-
-    let result = func(event).await.expect("failed to handle event");
-    assert_eq!(result, json!({
-        "message": "Hello, /!"
-    }));
-}
