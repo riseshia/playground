4648fbdfad9f38cb81cc8ef1b1ff90c9b1076d63
Shia <rise.shia@gmail.com>
2023-02-20 21:24:19 +0900

    Needless borrow

--- rust-in-action/actionkv/src/akv_mem.rs
+++ rust-in-action/actionkv/src/akv_mem.rs
@@ -20,9 +20,9 @@ Usage:
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let fname = args.get(1).expect(&USAGE);
-    let action = args.get(2).expect(&USAGE).as_ref();
-    let key = args.get(3).expect(&USAGE).as_ref();
+    let fname = args.get(1).expect(USAGE);
+    let action = args.get(2).expect(USAGE).as_ref();
+    let key = args.get(3).expect(USAGE).as_ref();
     let maybe_value = args.get(4);
 
     let path = std::path::Path::new(&fname);
@@ -36,13 +36,13 @@ fn main() {
         },
         "delete" => store.delete(key).unwrap(),
         "insert" => {
-            let value = maybe_value.expect(&USAGE).as_ref();
+            let value = maybe_value.expect(USAGE).as_ref();
             store.insert(key, value).unwrap()
         },
         "update" => {
-            let value = maybe_value.expect(&USAGE).as_ref();
+            let value = maybe_value.expect(USAGE).as_ref();
             store.update(key, value).unwrap()
         },
-        _ => eprintln!("{}", &USAGE),
+        _ => eprintln!("{}", USAGE),
     }
 }
