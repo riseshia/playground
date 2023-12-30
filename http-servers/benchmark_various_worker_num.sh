6992c42f2f5eb8a799759d5e840861322db5b5ec
Shia <rise.shia@gmail.com>
2023-12-30 18:51:59 +0900

    Check various worker num

--- http-servers/benchmark_various_worker_num.sh
+++ http-servers/benchmark_various_worker_num.sh
@@ -5,15 +5,22 @@ set -e
 export HOST=127.0.0.1
 export PORT=3000
 export PROCESS_COUNT=2
-export WORKER_PER_PROCESS_COUNT=2
 export RUBY_MAX_CPU=$PROCESS_COUNT
 
-servers="fiber single_threaded multi_threaded prefork prefork_multi_threaded ractor"
 # servers="fiber" segfault with high traffic
 servers="single_threaded multi_threaded prefork prefork_multi_threaded ractor"
-servers="single_threaded multi_threaded"
 
-for server in $servers; do
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <server>"
+  echo "  <server> is one of $servers"
+  exit 1
+fi
+
+server=$1
+
+for worker_num in $(seq 4 4 32); do
+  export WORKER_PER_PROCESS_COUNT=$worker_num
+
   echo "Benchmarking $server (process: $PROCESS_COUNT, worker per process: $WORKER_PER_PROCESS_COUNT)"
   export SERVER=$server
 
