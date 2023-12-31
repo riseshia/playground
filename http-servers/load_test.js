adc54bf0dee9fd3c24f71eb87e26189f85ba0754
Shia <rise.shia@gmail.com>
2023-12-31 13:27:42 +0900

    Use vus instead of constant rps

--- http-servers/load_test.js
+++ http-servers/load_test.js
@@ -1,15 +1,17 @@
 import http from 'k6/http';
 
 export const options = {
-  scenarios: {
-    open_model: {
-      executor: 'constant-arrival-rate',
-      rate: 100,
-      timeUnit: '1s',
-      duration: '10s',
-      preAllocatedVUs: 10,
-    },
-  },
+  vus: 32,
+  duration: '10s',
+  // scenarios: {
+  //   open_model: {
+  //     executor: 'constant-arrival-rate',
+  //     rate: 500,
+  //     timeUnit: '1s',
+  //     duration: '10s',
+  //     preAllocatedVUs: 50,
+  //   },
+  // },
 };
 
 export default function () {
