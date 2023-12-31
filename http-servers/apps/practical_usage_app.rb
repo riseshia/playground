c47d71ba0c763af98a0326845892e3ac4fa5aaeb
Shia <rise.shia@gmail.com>
2023-12-31 14:47:16 +0900

    Adjust practical usage app

--- http-servers/apps/practical_usage_app.rb
+++ http-servers/apps/practical_usage_app.rb
@@ -5,20 +5,20 @@ class PracticalUsageApp
 
   def call(_env)
     res_body = ""
-    # this is SLOWER with ractors
-    10.times do |i|
-      500.downto(1) do |j|
-        Math.sqrt(j) * i / 0.2
+    10.times do |_i|
+      # this is SLOWER with ractors
+      # 500.downto(1) do |j|
+      #   Math.sqrt(j) * i / 0.2
+      # end
+      1000.times do |i|
+        Math.sqrt(23467**2436) * i / 0.2
       end
+
       sleep 0.01
       partial = 1000.times.map { CHARS.sample }.join
       res_body += partial
       res_body += "\n"
     end
-
-    # 100.times do |i|
-    #   Math.sqrt(23467**2436) * i / 0.2
-    # end
 
     [200, { "Content-Type" => "text/html" }, [res_body]]
   end
