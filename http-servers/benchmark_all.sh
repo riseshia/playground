4a5c38cfb348e324799554e1f4f3b363fa06e9c4
Shia <rise.shia@gmail.com>
2023-12-31 14:44:54 +0900

    Adjust bench

--- http-servers/benchmark_all.sh
+++ http-servers/benchmark_all.sh
@@ -5,7 +5,7 @@ set -eu
 export HOST=127.0.0.1
 export PORT=3000
 export PROCESS_COUNT=1
-export MAX_PROCESS_NUM=2
+export MAX_PROCESS_NUM=4
 export WORKER_PER_PROCESS_COUNT=1
 export MAX_WORKER_NUM=32
 export APP=high_cpu
@@ -25,13 +25,13 @@ for worker_num in $(seq 8 8 $MAX_WORKER_NUM); do
 done
 
 # prefork
-for process_num in $(seq 1 1 $MAX_PROCESS_NUM); do
+for process_num in $(seq 2 1 $MAX_PROCESS_NUM); do
   export PROCESS_COUNT=$process_num
   SERVER=prefork ./benchmark.sh
 done
 
 # prefork_multi_threaded
-for process_num in $(seq 1 1 $MAX_PROCESS_NUM); do
+for process_num in $(seq 2 1 $MAX_PROCESS_NUM); do
   export PROCESS_COUNT=$process_num
 
   for worker_num in $(seq 8 8 $MAX_WORKER_NUM); do
@@ -41,7 +41,7 @@ for process_num in $(seq 1 1 $MAX_PROCESS_NUM); do
 done
 
 # ractor
-for process_num in $(seq 1 1 $MAX_PROCESS_NUM); do
+for process_num in $(seq 2 1 $MAX_PROCESS_NUM); do
   export PROCESS_COUNT=$process_num
 
   for worker_num in $(seq 8 8 $MAX_WORKER_NUM); do
