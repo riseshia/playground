68031a63ce0225d5ad38d014d4d10de4491f6356
Shia <rise.shia@gmail.com>
2024-02-27 23:10:49 +0900

    Ractor friendly Logger...?

--- ractor-test/logger_on_ractor.rb
+++ ractor-test/logger_on_ractor.rb
@@ -1,30 +1,83 @@
+# frozen_string_literal: true
+# shareable_constant_value: literal
+
 $stdout.sync = true
 
 require 'logger'
 
-module L
-  LoggerRactor = Ractor.new do
-    logger = ::Logger.new($stdout)
+# require 'nbproc'
+
+Message = Data.define(:msg, :event)
+
+class LoggerBackend
+  def self.start_server(*args, **kwargs)
+    Ractor.new(self, args.freeze, kwargs.freeze) do |klass, args, kwargs|
+      obj = klass.new(*args, **kwargs)
+
+      loop do
+        msg = Ractor.receive
+        obj.handle_call(msg)
+      end
+    end
+  end
+
+  def initialize(logdev)
+    @logger = Logger.new(logdev)
+  end
+
+  def handle_call(data)
+    @logger.send(data.msg, data.event)
+  end
+
+  def call(event)
+    @r.send(event)
+  end
+end
+
+module RactorLogger
+  module_function
+
+  BACKENDS_RACTOR = Ractor.new do
+    backends = []
 
     loop do
-      method, val = Ractor.receive
-      logger.send(method, val)
+      message = Ractor.receive
+
+      case message.msg
+      when :add_backend
+        backends << message.event
+      when :broadcast
+        backends.each do |backend|
+          backend << message.event
+        end
+      end
     end
   end
 
-  module Logger
-    module_function
-
-    define_method(:info) do |val|
-      LoggerRactor.send(:info, val)
-    end
+  def info(str)
+    msg = Message.new(:info, str)
+    BACKENDS_RACTOR << Message.new(:broadcast, msg)
   end
+
+  def add_backend(backend)
+    BACKENDS_RACTOR << Message.new(:add_backend, backend)
+  end
+end
+
+backend = LoggerBackend.start_server($stdout)
+RactorLogger.add_backend(backend)
+
+10.times.map do |i|
+  text = "Hello, Ractor! from main ractor#{i}"
+  RactorLogger.info(text)
+  msg = Message.new(:info, text)
+  backend.send(msg)
 end
 
 10.times.map do |i|
   Ractor.new(i) do |i|
-    L::Logger.info "Hello, Ractor! #{i}"
+    RactorLogger.info("Hello, Ractor! from sub ractor#{i}")
   end
 end
 
-sleep 1
+sleep 2
