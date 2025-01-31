8eeb48139a48b44d13b1445b5a4f969ca81ab7a9
Shia <rise.shia@gmail.com>
2025-01-31 18:12:44 +0900

    Reborn with exwiw

---
+++ exwiw/lib/exwiw.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "exwiw/version"
+
+module Exwiw
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ exwiw/lib/exwiw/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Exwiw
+  VERSION = "0.1.0"
+end
