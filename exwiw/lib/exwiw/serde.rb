a8a0515f86ba46ed95fe4ddac50e5090e178171c
Shia <rise.shia@gmail.com>
2025-02-02 14:48:18 +0900

    wip

--- exwiw/lib/exwiw/serde.rb
+++ exwiw/lib/exwiw/serde.rb
@@ -5,24 +5,74 @@ require_relative "serde/v"
 module Exwiw
   module Serde
     TypeError = Class.new(StandardError)
+    RequiredAttributeError = Class.new(StandardError)
+    DeclareError = Class.new(StandardError)
+
+    Boolean = [TrueClass, FalseClass].freeze
+
+    # OptionalType is used to mark an attribute as optional.
+    class OptionalType
+      attr_reader :base_type
+
+      def initialize(base_type)
+        @base_type = base_type
+      end
+    end
 
     def self.included(base)
       base.extend ClassMethods
     end
 
     module ClassMethods
+      def optional(type)
+        OptionalType.new(type)
+      end
+
       def attribute(name, type, options = {})
         attr_reader name
+
+        if type.is_a?(Array) && type.size != 1
+          raise Exwiw::Serde::DeclareError, "Array type must have exactly one type in the array"
+        end
 
         _serde_attrs[name] = { type: type, options: options }
         _serde_deserialize_keymap[name.to_s] = name
 
         define_method("#{name}=") do |value|
-          if value.is_a?(type)
-            instance_variable_set("@#{name}", value)
+          if type.is_a?(Exwiw::Serde::OptionalType)
+            # For optional attributes: nil is allowed, otherwise verify against the base_type.
+            if value.nil? || self.class.send(:valid_type?, value, type.base_type)
+              instance_variable_set("@#{name}", value)
+            else
+              raise Exwiw::Serde::TypeError, "Expected #{self.class}##{name} to be a #{type.base_type} (or nil), but got #{value.class}"
+            end
           else
-            raise TypeError, "Expected #{self.class}##{name} to be a #{type}, but got #{value.class}"
+            if value.nil?
+              raise Exwiw::Serde::TypeError, "Missing required attribute '#{name}' for #{self.class}"
+            elsif self.class.send(:valid_type?, value, type)
+              instance_variable_set("@#{name}", value)
+            else
+              raise Exwiw::Serde::TypeError, "Expected #{self.class}##{name} to be a #{type}, but got #{value.class}"
+            end
           end
+        end
+      end
+
+      def valid_type?(value, type)
+        if type == Boolean
+          # Boolean type: allow only true or false.
+          return value.is_a?(TrueClass) || value.is_a?(FalseClass)
+        elsif type.is_a?(Array)
+          if type.size == 1
+            # Single-element array type: value must be an Array and all elements must be instances of type.first.
+            return false unless value.is_a?(::Array)
+            return value.all? { |v| v.is_a?(type.first) }
+          else
+            # Multi-element array type: for union types like Boolean, check if value is an instance of any type.
+            return type.any? { |t| value.is_a?(t) }
+          end
+        else
+          value.is_a?(type)
         end
       end
 
@@ -34,25 +84,34 @@ module Exwiw
         @_serde_deserialize_keymap ||= {}
       end
 
-      def from(obj)
-        method_name = build_from_method_name(obj)
-
-        if obj.class == self.class
-          obj.clone
-        elsif respond_to?(method_name)
-          __send__(method_name, obj)
-        else
-          raise "Naiyo"
-        end
-      end
-
       def from_hash(from_obj)
         new.tap do |obj|
           _serde_deserialize_keymap.each do |from_key, to_key|
             from_value = from_obj[from_key]
-            obj.__send__("#{to_key}=", from_value)
+            to_type = _serde_attrs[to_key][:type]
+
+            if from_value.class == to_type
+              obj.__send__("#{to_key}=", from_value.dup)
+              next
+            end
+
+            if to_type.is_a?(Array)
+              el_type = to_type.first
+              method_name = build_from_method_name(from_value)
+            else
+              method_name = build_from_method_name(from_value)
+              if to_type.respond_to?(method_name)
+                value = to_type.__send__(method_name, from_value)
+                obj.__send__("#{to_key}=", value)
+              else
+                raise "#{to_type} does not respond to #{method_name}"
+              end
+            end
           end
         end
+      end
+
+      private def deserialize(from_obj, to_type)
       end
 
       def to(target_class = Hash)
@@ -75,10 +134,6 @@ module Exwiw
       def build_to_method_name(obj)
         suffix = obj.class.name.downcase.gsub("::", "__")
         "to_#{suffix}"
-      end
-
-      private def _serde_attrs
-        @_serde_attrs ||= {}
       end
     end
   end
--- exwiw/spec/serde_spec.rb
+++ exwiw/spec/serde_spec.rb
@@ -1,43 +1,94 @@
 # frozen_string_literal: true
 
 RSpec.describe Exwiw::Serde do
+  class Database
+    include Exwiw::Serde
+
+    attribute :adapter, String
+  end
+
+  class Table
+    include Exwiw::Serde
+
+    attribute :name, String
+    attribute :comment, optional(String)
+  end
+
   class Config
     include Exwiw::Serde
 
     attribute :name, String
+    attribute :version, Integer
+    attribute :database, Database
+    attribute :tables, [Table]
   end
 
   context "deserialize" do
     it "does not raise error" do
       expect {
-        Config.from({ "name" => "config" })
+        Config.from_hash({
+          "name" => "config",
+          "version" => 1,
+          "database" => {
+            "adapter" => "mysql"
+          },
+          "tables" => [
+            { "name" => "users" },
+            { "name" => "posts", "comment" => "This is a comment" }
+          ]
+        })
       }.not_to raise_error
     end
 
-    it "raises error" do
-      expect {
-        Config.from({ "name" => 111 })
-      }.to raise_error(Exwiw::Serde::TypeError)
-    end
-
-    it "builds config correctly" do
-      config = Config.from({ "name" => "config" })
-
-      expect(config.name).to eq("config")
-    end
+    # it "raises error" do
+    #   expect {
+    #     Config.from({
+    #       "name" => "config",
+    #       "database" => {
+    #         "adapter" => "mysql"
+    #       },
+    #       "tables" => [
+    #         { "name" => "users" },
+    #         { "name" => "posts", "comment" => "This is a comment" }
+    #       ]
+    #     })
+    #   }.to raise_error(Exwiw::Serde::RequiredAttributeError)
+    # end
+    #
+    # it "builds config correctly" do
+    #   config = Config.from({
+    #     "name" => "config",
+    #     "version" => 1,
+    #     "database" => {
+    #       "adapter" => "mysql"
+    #     },
+    #     "tables" => [
+    #       { "name" => "users" },
+    #       { "name" => "posts", "comment" => "This is a comment" }
+    #     ]
+    #   })
+    #
+    #   expect(config.name).to eq("config")
+    #   expect(config.version).to eq(1)
+    #   expect(config.database.adapter).to eq("mysql")
+    #   expect(config.tables.first.name).to eq("users")
+    #   expect(config.tables.first.comment).to be_nil
+    #   expect(config.tables.last.name).to eq("posts")
+    #   expect(config.tables.last.comment).to eq("This is a comment")
+    # end
   end
 
-  context "serialize" do
-    it "does not raise error" do
-      expect {
-        Config.to({ "name" => "config" }).to
-      }.not_to raise_error
-    end
+  # context "serialize" do
+  #   it "does not raise error" do
+  #     expect {
+  #       Config.to({ "name" => "config" }).to
+  #     }.not_to raise_error
+  #   end
 
-    it "builds hash correctly" do
-      config = Config.from({ "name" => "config" })
+  #   it "builds hash correctly" do
+  #     config = Config.from({ "name" => "config" })
 
-      expect(config.to).to eq({ "name" => "config" })
-    end
-  end
+  #     expect(config.to).to eq({ "name" => "config" })
+  #   end
+  # end
 end
